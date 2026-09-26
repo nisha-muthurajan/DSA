372611ddf1b0602c826733cb7a7d18758fe5f738
nisha-muthurajan <deltadelta5382@gmail.com>
2026-09-26 06:21:37 +0530

    Update docstring to clarify functionality and provide example for maximum consecutive 1's calculation

--- dsa hometask/day10/max_consecutive_ones3.py
+++ dsa hometask/day10/max_consecutive_ones3.py
@@ -1,19 +1,13 @@
-"""class Solution:
-    def longestOnes(self, nums: list[int], k: int) -> int:
-        maximum=0
-        left=0
-        zero_count=0
-        for right in range(len(nums)):
-            if nums[right]==0:
-                zero_count+=1
+"""Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
 
-            while zero_count>k:
-                if nums[left]==0:
-                    zero_count-=1
-                left+=1
+ 
 
-            maximum=max(maximum,right-left+1)
-        return maximum"""
+Example 1:
+
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined."""
 
 nums=list(map(int,input().split()))
 k=int(input())
